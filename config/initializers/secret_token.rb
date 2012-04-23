d32399626d50838b87777e99f333df7a1303d9c8
Tomiwa Ijaware <e911miri@gmail.com>
2012-04-23 21:12:50 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Radrails2::Application.config.secret_token = '1935857ab4dead046443418bb4bc52d27524747b1534ee667dbb397c743c1984f2ea5c4536e42034f78f55434c2aefc1d48233d46c6251080a0c4d1fadade79b'
