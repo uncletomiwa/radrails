c192768ee2f202cfa6668d42953468a078c954d5
Tomiwa Ijaware <e911miri@gmail.com>
2012-05-04 23:57:03 +0100

    Testing commit

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
@@ -6,13 +6,4 @@ def full_title(page_title)
   else
     "#{base_title} | #{page_title}"
   end
-=======
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-  base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
->>>>>>> filling-in-layout
 end
